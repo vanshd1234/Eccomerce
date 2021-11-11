b43a8894b441313b2b951e54cf00f0275cfb5e8e
vanshd1234 <pradhanvanshdeep1@gmail.com>
2021-11-11 19:54:50 +0530

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Admin;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.login');
+    }
+
+    public function auth(Request $request)
+    {
+        $email=$request->post('email');
+        $password=$request->post('password');
+
+        $result=Admin::where(['email'=>$email,'password'=>$password])->get();
+        if(isset($result['0']->id)){
+            $request->session()->put('ADMIN_LOGIN',true);
+            $request->session()->put('ADMIN_ID',$result['0']->id);
+            return redirect('admin/dashboard');
+
+        }else{
+            $request->session()->flash('error','Please use valid mail');
+            return redirect('admin');
+        }
+    }
+
+    public function dashboard(){
+        return view('admin.dashboard');
+    }
+    
+    public function category(){
+        return view('admin.category');
+    }
+}
